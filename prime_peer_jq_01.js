659abf3b36f62bd39717cae78d228d13ca2a3746
5nahalf <5nahalf@comcast.net>
2015-04-20 14:55:11 -0500

    add function

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -1,5 +1,35 @@
+$(document).ready(function(){
+$("#btn").on("click", function(){
+	People();
+	$("#person").append("<p>You are a " + gender + " who is " + weight + "lbs and " + age + " years old</p>");
+
+	});
+});
+
+function People(age, sex, weight){
+	this.sex = randomNumber(0, 1);
+	this.weight = randomNumber(1, 200);
+	this.age = randomNumber(1, 100);
+	if (this.sex == 0){
+			gender = "Male";
+		} else {
+			gender = "Female"
+		}
+}
+
+
+//if(People.sex = 0){
+//		gender = "Male";
+//	}if(People.sex = 1) {
+//		gender = "Female"
+//	}
+
+//console.log();
+
 //Utility function
 //A simple random number generator
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+
